1615b7a6706f58cfceeb054d35d9c7ce9eb6b969
Soumyadeep Ghosh <ghoshsoumyadeep3@gmail.com>
2025-08-01 23:28:59 +0530

    done home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -12,5 +13,5 @@ import {MatButton} from '@angular/material/button';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+ 
 }
