6f91837a5a80dc854377892c9b0757da2e15d13f
Soumyadeep Ghosh <ghoshsoumyadeep3@gmail.com>
2025-08-01 17:51:21 +0530

    Initial version

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    MatIcon,
+    MatButton
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
