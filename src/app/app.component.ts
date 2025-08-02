2ad50b6ae1b5b7c85d9d4b3b0803205202e25ea7
Soumyadeep Ghosh <ghoshsoumyadeep3@gmail.com>
2025-08-02 22:32:42 +0530

    water ripple effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { VantaBackgroundComponent } from "./components/vanta-background/vanta-background.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, VantaBackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class App {
   protected readonly title = signal('portfolio2');
 }
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton} from '@angular/material/button';
-import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   imports: [
     MatIcon,
-    MatButton
-  ],
+    MatButton,
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
