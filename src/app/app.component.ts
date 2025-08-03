edabee60c8b334c4c6dc299297db6c3b39791b37
Soumyadeep Ghosh <ghoshsoumyadeep3@gmail.com>
2025-08-03 23:39:25 +0530

    added projects/contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { WorkSectionComponent } from "./components/work-section/work-section.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, WorkSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, WorkSectionComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/work-section/work-section.component.ts
+++ src/app/components/work-section/work-section.component.ts
@@ -8,9 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './work-section.component.scss'
 })
 export class WorkSectionComponent {
-   activeTab: string = 'experience';
-
+  activeTab: string = 'experience';
+  selectedExperience: any = null;
   setTab(tab: string) {
     this.activeTab = tab;
+  }
+
+  openModal(role: string) {
+    const experiences: any = {
+      technicalLead: {
+        title: 'Technical Lead',
+        company: 'LTIMindtree',
+        duration: '2022 – 2023',
+        details: 'Provided L2/L3 support, managed incidents via ServiceNow/JIRA, led a team, and monitored applications using SolarWinds.'
+      },
+      fullStackDev: {
+        title: 'Full Stack Developer',
+        company: 'Cognizant',
+        duration: '2021 – 2022',
+        details: 'Migrated .NET services to 4.8, developed travel apps, resolved incidents within sprints using SQL Server, Web API 2.0, and Entity Framework.'
+      },
+      technicalAnalyst: {
+        title: 'Technical Analyst',
+        company: 'Cognizant',
+        duration: '2017 – 2021',
+        details: 'Delivered L2/L3 support, stabilized platforms, and built expertise in AngularJS, ASP.NET Web API, and ServiceNow.'
+      },
+      programmerTrainee: {
+        title: 'Analyst Trainee',
+        company: 'Cognizant',
+        duration: '2015 – 2017',
+        details: 'Gained foundational experience with .NET, SQL Server, and application support in the insurance domain.'
+      }
+    };
+    this.selectedExperience = experiences[role];
+  }
+
+  closeModal() {
+    this.selectedExperience = null;
   }
 }
